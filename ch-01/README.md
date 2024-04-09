342754aaf14723af4c71731fddba489112bd486c
Arun Sundaram <arun_co@yahoo.com>
2024-04-09 18:06:16 +0000

    updates

---
+++ ch-01/README.md
@@ -0,0 +1,1 @@
+# Program to print hello world
---
+++ ch-02/args-list.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+// program to print input arguments
+func main() {
+	// get input arguments
+	args := os.Args[1:]
+	// range over each input argument print it
+	for _, arg := range args {
+		fmt.Println(arg)
+	}
+}
